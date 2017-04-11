2b6a0b7e6261ac5cb48be21da577f4a5dfb76d0b
JRSuckert <jens.suckert@gmail.com>
2017-04-11 13:48:00 +0200

    Bugfix

--- source/mod_lattice.F90
+++ source/mod_lattice.F90
@@ -220,7 +220,7 @@ contains
     integer :: i
 
     open(unit = 5675, file='positions', status='replace')
-    write(5675, "('In plane basis: '3(f12.9,2x)' '3f(12.9,2x))") a1_pln, a2_pln
+    write(5675, "('In plane basis: '3(f12.9,2x)' '3(f12.9,2x))") a1_pln, a2_pln
 
     write(5675, *) "# Number   Position   Dir. Cosine"
     write(5675, *) "# In-plane neighbors"
@@ -231,17 +231,17 @@ contains
 
     write(5675, *) "# 1st out-of-plane neighbors"
     do i=1,n1
-       write(5675, "(''I0' '3(f12.9,2x)' '3(f12.9,2x)'')") i, r0(i,:), c0(i,:)
+       write(5675, "(''I0' '3(f12.9,2x)' '3(f12.9,2x)'')") i, r1(i,:), c1(i,:)
     end do
 
     write(5675, *) "# 2nd out-of-plane neighbors"
     do i=1,n2
-       write(5675, "(''I0' '3(f12.9,2x)' '3(f12.9,2x)'')") i, r0(i,:), c0(i,:)
+       write(5675, "(''I0' '3(f12.9,2x)' '3(f12.9,2x)'')") i, r2(i,:), c2(i,:)
     end do
 
     close(unit = 5675)
 
     return
-  end subroutine write_kpoints_to_file
+  end subroutine write_positions_to_file
 
 end module mod_lattice
