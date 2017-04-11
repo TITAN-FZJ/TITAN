cc33f40c3f50ae9af67c2e1923bc6ef85c4aa087
JRSuckert <jens.suckert@gmail.com>
2017-04-11 10:42:34 +0200

    Added subroutine to write positions

--- source/mod_lattice.F90
+++ source/mod_lattice.F90
@@ -213,4 +213,35 @@ contains
 
     return
   end subroutine next_neighbour_init
+
+  subroutine write_positions_to_file()
+    use mod_parameters, only: a1_pln, a2_pln
+    implicit none
+    integer :: i
+
+    open(unit = 5675, file='positions', status='replace')
+    write(5675, "('In plane basis: '3(f12.9,2x)' '3f(12.9,2x))") a1_pln, a2_pln
+
+    write(5675, *) "# Number   Position   Dir. Cosine"
+    write(5675, *) "# In-plane neighbors"
+
+    do i=1,n0
+       write(5675, "(''I0' '3(f12.9,2x)' '3(f12.9,2x)'')") i, r0(i,:), c0(i,:)
+    end do
+
+    write(5675, *) "# 1st out-of-plane neighbors"
+    do i=1,n1
+       write(5675, "(''I0' '3(f12.9,2x)' '3(f12.9,2x)'')") i, r0(i,:), c0(i,:)
+    end do
+
+    write(5675, *) "# 2nd out-of-plane neighbors"
+    do i=1,n2
+       write(5675, "(''I0' '3(f12.9,2x)' '3(f12.9,2x)'')") i, r0(i,:), c0(i,:)
+    end do
+
+    close(unit = 5675)
+
+    return
+  end subroutine write_kpoints_to_file
+
 end module mod_lattice
