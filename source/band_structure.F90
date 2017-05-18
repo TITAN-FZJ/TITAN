eb14e447645fbf883827f06ba481c8999c7538a7
suckert <jens.suckert@gmail.com>
2017-05-18 10:57:26 +0200

    Bugfix

--- source/band_structure.F90
+++ source/band_structure.F90
@@ -1,3 +1,64 @@
+subroutine read_band_points(kbands)
+  use mod_parameters, only: bands, band_cnt
+  use mod_system, only: b1, b2, b3
+  use mod_f90_kind, only: double
+  implicit none
+  real(double), dimension(:,:), allocatable, intent(out) :: kbands
+  character(len=40) :: band_file = "kbands"
+  character(len=200) :: line
+  integer :: ios, line_count, i, j
+  logical :: found
+  type :: band_point
+    character(len=5) :: name
+    real(double), dimension(3) :: kp
+  end type
+  type(band_point), dimension(:), allocatable :: kband
+
+  line_count = 0
+  open(unit = 666999, file = trim(band_file), status='old', iostat=ios)
+  if(ios /= 0) then
+    !TODO: Call upon hell
+  endif
+
+  ! Count non commented lines
+  read(unit=666999, fmt='(A)', iostat=ios) line
+  do while (ios == 0)
+    if(len_trim(line) > 0 .and. len_trim(line) < 200 .and. index(adjustl(line), "#") /= 1) line_count = line_count + 1
+    read(unit=666999, fmt='(A)', iostat=ios) line
+  end do
+  rewind(666999)
+
+  allocate(kband(line_count))
+  i = 0
+  read(unit=666999, fmt='(A)', iostat=ios) line
+  do while (ios == 0)
+    if(len_trim(line) <= 0 .or. len_trim(line) >= 200 .or. index(adjustl(line),"#") == 1) cycle
+    if(index(line, "#") > 0) line = line(1:index(line, "#")-1)
+    i = i + 1
+    read(unit = line, fmt = *, iostat=ios) kband(i)%name, (kband(i)%kp(j), j = 1,3)
+    read(unit=666999, fmt='(A)', iostat=ios) line
+  end do
+
+  allocate(kbands(3,band_cnt))
+  do i = 1, band_cnt
+    found = .false.
+    do j = 1, line_count
+      if(trim(kband(j)%name) == trim(bands(i))) then
+        found = .true.
+        kbands(1:3,i) = kband(j)%kp(1) * b1 + kband(j)%kp(2) * b2 + kband(j)%kp(3) * b3
+        exit
+      endif
+    end do
+    if(.not. found) then
+      print *, "read_bands went wrong"
+      stop
+    endif
+  end do
+
+  close(666999)
+end subroutine read_band_points
+
+
 !   Calculates magnetic LDOS
 subroutine band_structure()
   use mod_f90_kind
@@ -9,14 +70,21 @@ subroutine band_structure()
   character(len=400) :: varm
   character(len=50)  :: fieldpart,socpart
   character(len=1)   :: SOCc
-  integer            :: i, j,ifail
+  integer            :: i, j, ifail
   integer                       :: lwork,dimbs
-  real(double)                  :: kmin(3),kmax(3), dir(3), deltak
+  real(double)                  :: dir(3), deltak
   real(double),allocatable      :: rwork(:),kpoints(:,:)
   complex(double),allocatable   :: eval(:),evecl(:,:),evecr(:,:),work(:)
   complex(double),allocatable   :: hk(:,:)
   real(double), dimension(:,:), allocatable :: band_points
   real(double) :: total_length
+
+    interface
+      subroutine read_band_points(kbands)
+        use mod_f90_kind, only: double
+        real(double), dimension(:,:), allocatable, intent(out) :: kbands
+      end subroutine
+    end interface
 
   dimbs = (Npl_total)*18
   lwork = 33*dimbs
@@ -60,16 +128,15 @@ subroutine band_structure()
   count = 0
   i = 0
   do count = 1, npt1
-    if(deltak*count >= total_length) then
+    if(deltak*count >= total_length .and. i < band_cnt - 1) then
       i = i + 1
-      total_length = total_length + sqrt(dot_product(band_points(:,i)-band_points(:,i+1), band_points(:,i)-band_points(:,i+1)))
+      total_length = total_length + sqrt(dot_product(band_points(1:3,i)-band_points(1:3,i+1), band_points(1:3,i)-band_points(1:3,i+1)))
       j = 0
-      dir = (band_points(:,i+1) - band_points(:,i))
+      dir = (band_points(1:3,i+1) - band_points(1:3,i))
       dir = dir / sqrt(dot_product(dir,dir))
     end if
     kpoints(:,count) = band_points(:,i) + dir * j * deltak
     j = j + 1
-    print *, kpoints(:,count)
   end do
 
   ! deltak = (kmax - kmin)/npts
@@ -98,64 +165,3 @@ subroutine band_structure()
 
   return
 end subroutine band_structure
-
-subroutine read_band_points(kbands)
-  use mod_parameters, only: bands, band_cnt
-  use mod_system, only: b1, b2, b3
-  use mod_f90_kind, only: double
-  implicit none
-  real(double), dimension(:,:), allocatable, intent(out) :: kbands
-  character(len=40) :: band_file = "kbands"
-  character(len=200) :: line
-  integer :: ios, line_count, i, j
-  logical :: found
-  type :: band_point
-    character(len=5) :: name
-    real(double), dimension(3) :: kp
-  end type
-  type(band_point), dimension(:), allocatable :: kband
-
-  line_count = 0
-  open(unit = 666999, file = trim(band_file), status='old', iostat=ios)
-  if(ios /= 0) then
-    !TODO: Call upon hell
-  endif
-
-  ! Count non commented lines
-  read(unit=666999, fmt='(A)', iostat=ios) line
-  do while (ios == 0)
-    if(len_trim(line) > 0 .and. len_trim(line) < 200 .and. index(adjustl(line), "#") /= 1) line_count = line_count + 1
-    read(unit=666999, fmt='(A)', iostat=ios) line
-  end do
-  rewind(666999)
-  print *, line_count
-
-  allocate(kband(line_count))
-  i = 0
-  read(unit=666999, fmt='(A)', iostat=ios) line
-  do while (ios == 0)
-    if(len_trim(line) <= 0 .or. len_trim(line) >= 200 .or. index(adjustl(line),"#") == 1) cycle
-    if(index(line, "#") > 0) line = line(1:index(line, "#")-1)
-    i = i + 1
-    read(unit = line, fmt = *, iostat=ios) kband(i)%name, (kband(i)%kp(j), j = 1,3)
-    read(unit=666999, fmt='(A)', iostat=ios) line
-  end do
-
-  allocate(kbands(3,band_cnt))
-  do i = 1, band_cnt
-    found = .false.
-    do j = 1, line_count
-      if(trim(kband(j)%name) == bands(i)) then
-        found = .true.
-        kbands(1:3,i) = kband(j)%kp(1) * b1 + kband(j)%kp(2) * b2 + kband(j)%kp(3) * b3
-        exit
-      endif
-    end do
-    if(.not. found) then
-      print *, "read_bands went wrong"
-      stop
-    endif
-  end do
-
-  close(666999)
-end subroutine read_band_points
