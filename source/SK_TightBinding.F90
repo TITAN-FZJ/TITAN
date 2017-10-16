0409ef15a6846da7dc6b40312448650e4822b40a
Filipe Guimaraes <f.guimaraes@fz-juelich.de>
2017-10-16 17:01:41 +0200

    Changed order and name of intd arguments to the same as Papaconstantopolus.

--- source/SK_TightBinding.F90
+++ source/SK_TightBinding.F90
@@ -71,7 +71,7 @@ contains
     do i = 1, 10
       mix(i) = sign(sqrt(abs(t1(i)) * abs(t2(i))), t1(i) + t2(i))
     end do
-    call intd(mix(1), mix(7), mix(2), mix(3), mix(8), mix(9), mix(10), mix(4), mix(5), mix(6), dirCos, t0i)
+    call intd(mix(1), mix(2), mix(3), mix(4), mix(5), mix(6), mix(7), mix(8), mix(9), mix(10), dirCos, t0i)
     return
   end subroutine set_hopping_matrix
 
@@ -157,11 +157,11 @@ contains
     close(995594)
   end subroutine
 
-  pure subroutine intd(sss,sps,pps,ppp,ss,ps,pp,ds,dp,dd,w,b)
+  pure subroutine intd(sss,pps,ppp,dds,ddp,ddd,sps,sds,pds,pdp,w,b)
     use mod_f90_kind, only: double
     use mod_constants, only: sq3
     implicit none
-    real(double), intent(in)  :: sss,sps,pps,ppp,ss,ps,pp,ds,dp,dd,w(3)
+    real(double), intent(in)  :: sss,sps,pps,ppp,sds,pds,pdp,dds,ddp,ddd,w(3)
     real(double), intent(out) :: b(9,9)
     real(double)  :: x,y,z,xx,xy,yy,yz,zz,zx,xxyy,yyzz,zzxx
     real(double)  :: aux,aux1,aux2,aux3,aux4,r3,f1,f2,f3,f4,f5,f8,g1,g2
@@ -184,17 +184,17 @@ contains
     f2=xx-yy
     f3=zz-.5d0*f1
     f8=3.d0*zz-1.d0
-    f4=.5d0*r3*f2*ps
-    f5=.5d0*f8*ps
+    f4=.5d0*r3*f2*pds
+    f5=.5d0*f8*pds
 
-    g1=1.5d0*f2*ds
-    g2=r3*f3*ds
+    g1=1.5d0*f2*dds
+    g2=r3*f3*dds
 
     aux=pps-ppp
-    aux1=r3*ss
-    aux2=r3*xx*ps+(1.d0-2.d0*xx)*pp
-    aux3=(r3*yy*ps+(1.d0-2.d0*yy)*pp)
-    aux4=r3*zz*ps+(1.d0-2.d0*zz)*pp
+    aux1=r3*sds
+    aux2=r3*xx*pds+(1.d0-2.d0*xx)*pdp
+    aux3=(r3*yy*pds+(1.d0-2.d0*yy)*pdp)
+    aux4=r3*zz*pds+(1.d0-2.d0*zz)*pdp
 
     b(1,1)=sss
     b(1,2)=x*sps
@@ -204,17 +204,17 @@ contains
     b(1,6)=yz*aux1
     b(1,7)=zx*aux1
     b(1,8)=.5d0*f2*aux1
-    b(1,9)=.5d0*f8*ss
+    b(1,9)=.5d0*f8*sds
 
     b(2,1)=-b(1,2)
     b(2,2)=xx*pps+(1.d0-xx)*ppp
     b(2,3)=xy*aux
     b(2,4)=zx*aux
     b(2,5)=aux2*y
-    b(2,6)=(r3*ps-2.d0*pp)*xy*z
+    b(2,6)=(r3*pds-2.d0*pdp)*xy*z
     b(2,7)=aux2*z
-    b(2,8)=(f4+(1.d0-f2)*pp)*x
-    b(2,9)=(f5-r3*zz*pp)*x
+    b(2,8)=(f4+(1.d0-f2)*pdp)*x
+    b(2,9)=(f5-r3*zz*pdp)*x
 
     b(3,1)=-b(1,3)
     b(3,2)= b(2,3)
@@ -223,8 +223,8 @@ contains
     b(3,5)=aux3*x
     b(3,6)=aux3*z
     b(3,7)= b(2,6)
-    b(3,8)=(f4-(1.d0+f2)*pp)*y
-    b(3,9)=(f5-r3*zz*pp)*y
+    b(3,8)=(f4-(1.d0+f2)*pdp)*y
+    b(3,9)=(f5-r3*zz*pdp)*y
 
     b(4,1)=-b(1,4)
     b(4,2)= b(2,4)
@@ -233,28 +233,28 @@ contains
     b(4,5)= b(2,6)
     b(4,6)=aux4*y
     b(4,7)=aux4*x
-    b(4,8)=(f4-f2*pp)*z
-    b(4,9)=(f5+r3*f1*pp)*z
+    b(4,8)=(f4-f2*pdp)*z
+    b(4,9)=(f5+r3*f1*pdp)*z
 
     b(5,1)= b(1,5)
     b(5,2)=-b(2,5)
     b(5,3)=-b(3,5)
     b(5,4)=-b(4,5)
-    b(5,5)=3.d0*xxyy*ds+(f1-4.d0*xxyy)*dp+(zz+xxyy)*dd
-    b(5,6)=(3.d0*yy*ds+(1.d0-4.d0*yy)*dp+(yy-1.d0)*dd)*zx
-    b(5,7)=(3.d0*xx*ds+(1.d0-4.d0*xx)*dp+(xx-1.d0)*dd)*yz
-    b(5,8)=(g1-2.d0*f2*dp+.5d0*f2*dd)*xy
-    b(5,9)=(g2-2.d0*r3*zz*dp+.5d0*r3*(1.d0+zz)*dd)*xy
+    b(5,5)=3.d0*xxyy*dds+(f1-4.d0*xxyy)*ddp+(zz+xxyy)*ddd
+    b(5,6)=(3.d0*yy*dds+(1.d0-4.d0*yy)*ddp+(yy-1.d0)*ddd)*zx
+    b(5,7)=(3.d0*xx*dds+(1.d0-4.d0*xx)*ddp+(xx-1.d0)*ddd)*yz
+    b(5,8)=(g1-2.d0*f2*ddp+.5d0*f2*ddd)*xy
+    b(5,9)=(g2-2.d0*r3*zz*ddp+.5d0*r3*(1.d0+zz)*ddd)*xy
 
     b(6,1)= b(1,6)
     b(6,2)=-b(2,6)
     b(6,3)=-b(3,6)
     b(6,4)=-b(4,6)
     b(6,5)= b(5,6)
-    b(6,6)=3.d0*yyzz*ds+(yy+zz-4.d0*yyzz)*dp+(xx+yyzz)*dd
-    b(6,7)=(3.d0*zz*ds+(1.d0-4.d0*zz)*dp+(zz-1.d0)*dd)*xy
-    b(6,8)=(g1-(1.d0+2.d0*f2)*dp+(1.d0+.5d0*f2)*dd)*yz
-    b(6,9)=(g2+r3*(f1-zz)*dp-.5d0*r3*f1*dd)*yz
+    b(6,6)=3.d0*yyzz*dds+(yy+zz-4.d0*yyzz)*ddp+(xx+yyzz)*ddd
+    b(6,7)=(3.d0*zz*dds+(1.d0-4.d0*zz)*ddp+(zz-1.d0)*ddd)*xy
+    b(6,8)=(g1-(1.d0+2.d0*f2)*ddp+(1.d0+.5d0*f2)*ddd)*yz
+    b(6,9)=(g2+r3*(f1-zz)*ddp-.5d0*r3*f1*ddd)*yz
 
     b(7,1)= b(1,7)
     b(7,2)=-b(2,7)
@@ -262,9 +262,9 @@ contains
     b(7,4)=-b(4,7)
     b(7,5)= b(5,7)
     b(7,6)= b(6,7)
-    b(7,7)=3.d0*zzxx*ds+(zz+xx-4.d0*zzxx)*dp+(yy+zzxx)*dd
-    b(7,8)=(g1+(1.d0-2.d0*f2)*dp-(1.d0-.5d0*f2)*dd)*zx
-    b(7,9)=(g2+r3*(f1-zz)*dp-.5d0*r3*f1*dd)*zx
+    b(7,7)=3.d0*zzxx*dds+(zz+xx-4.d0*zzxx)*ddp+(yy+zzxx)*ddd
+    b(7,8)=(g1+(1.d0-2.d0*f2)*ddp-(1.d0-.5d0*f2)*ddd)*zx
+    b(7,9)=(g2+r3*(f1-zz)*ddp-.5d0*r3*f1*ddd)*zx
 
     b(8,1)= b(1,8)
     b(8,2)=-b(2,8)
@@ -273,8 +273,8 @@ contains
     b(8,5)= b(5,8)
     b(8,6)= b(6,8)
     b(8,7)= b(7,8)
-    b(8,8)=.75d0*f2*f2*ds+(f1-f2*f2)*dp+(zz+.25d0*f2*f2)*dd
-    b(8,9)=.5d0*f2*g2-r3*zz*f2*dp+.25d0*r3*(1.d0+zz)*f2*dd
+    b(8,8)=.75d0*f2*f2*dds+(f1-f2*f2)*ddp+(zz+.25d0*f2*f2)*ddd
+    b(8,9)=.5d0*f2*g2-r3*zz*f2*ddp+.25d0*r3*(1.d0+zz)*f2*ddd
 
     b(9,1)= b(1,9)
     b(9,2)=-b(2,9)
@@ -284,7 +284,7 @@ contains
     b(9,6)= b(6,9)
     b(9,7)= b(7,9)
     b(9,8)= b(8,9)
-    b(9,9)=f3*f3*ds+3.d0*zz*f1*dp+.75d0*f1*f1*dd
+    b(9,9)=f3*f3*dds+3.d0*zz*f1*ddp+.75d0*f1*f1*ddd
 
     return
   end subroutine intd
