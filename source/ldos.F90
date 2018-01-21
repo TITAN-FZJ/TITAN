0f3fe45be2567abf42e0fb3a2b0158caaa5fae96
Filipe Guimaraes <f.guimaraes@fz-juelich.de>
2018-01-22 00:01:09 +0100

    Forgot to open the files

--- source/ldos.F90
+++ source/ldos.F90
@@ -18,6 +18,7 @@ subroutine ldos()
   if(rField == 0) then
     write(output%unit_loop,"('CALCULATING LDOS')")
     if(.not.laddresults) call createLDOSFiles()
+    call openLDOSFiles()
   end if
 
   do i = startFreq, endFreq
@@ -29,18 +30,18 @@ subroutine ldos()
         if(rFreq(2) == 0) then
            do j = 1, sFreq(2)
              if (j /= 1) then
-               call MPI_Recv(e,     1                        ,MPI_DOUBLE_PRECISION,MPI_ANY_SOURCE  ,1000,FreqComm(2),stat,ierr)
-               call MPI_Recv(ldosd, s%nAtoms*nOrb            ,MPI_DOUBLE_PRECISION,stat(MPI_SOURCE),1100,FreqComm(2),stat,ierr)
-               call MPI_Recv(ldosu, s%nAtoms*nOrb            ,MPI_DOUBLE_PRECISION,stat(MPI_SOURCE),1200,FreqComm(2),stat,ierr)
+               call MPI_Recv(e,     1            ,MPI_DOUBLE_PRECISION,MPI_ANY_SOURCE  ,1000,FreqComm(2),stat,ierr)
+               call MPI_Recv(ldosd, s%nAtoms*nOrb,MPI_DOUBLE_PRECISION,stat(MPI_SOURCE),1100,FreqComm(2),stat,ierr)
+               call MPI_Recv(ldosu, s%nAtoms*nOrb,MPI_DOUBLE_PRECISION,stat(MPI_SOURCE),1200,FreqComm(2),stat,ierr)
                end if
 
                ! Writing into files
                call writeLDOS(e)
             end do
          else
-            call MPI_Recv(e,     1                        ,MPI_DOUBLE_PRECISION,0,1000,FreqComm(2),stat,ierr)
-            call MPI_Recv(ldosd, s%nAtoms*nOrb            ,MPI_DOUBLE_PRECISION,0,1100,FreqComm(2),stat,ierr)
-            call MPI_Recv(ldosu, s%nAtoms*nOrb            ,MPI_DOUBLE_PRECISION,0,1200,FreqComm(2),stat,ierr)
+            call MPI_Recv(e,     1            ,MPI_DOUBLE_PRECISION,0,1000,FreqComm(2),stat,ierr)
+            call MPI_Recv(ldosd, s%nAtoms*nOrb,MPI_DOUBLE_PRECISION,0,1100,FreqComm(2),stat,ierr)
+            call MPI_Recv(ldosu, s%nAtoms*nOrb,MPI_DOUBLE_PRECISION,0,1200,FreqComm(2),stat,ierr)
          end if
       end if
       call MPI_Barrier(FieldComm, ierr)
