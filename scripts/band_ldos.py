e1cd23af940e2f3ddb20a6e1f169ac5ba1ffe7b5
Filipe Guimaraes <f.guimaraes@fz-juelich.de>
2018-03-03 13:22:33 +0100

    Added parameter file for Au; Removed t2g and eg from band_ldos script.

--- scripts/band_ldos.py
+++ scripts/band_ldos.py
@@ -70,30 +70,22 @@ if(len(sys.argv)==4):
   tot1 = dat1[:,1]
   s1 = dat1[:,2]
   p1 = dat1[:,3]
-  t2g1 = dat1[:,4]
-  eg1 = dat1[:,5]
-  d1 = [x[0]+x[1] for x in zip(dat1[:,4],dat1[:,5]) ]
+  d1 = dat1[:,4]
 
   x2 = [ x-fermi for x in dat2[:,0] ]
   tot2 = [-x for x in dat2[:,1] ]
   s2 = [-x for x in dat2[:,2] ]
   p2 = [-x for x in dat2[:,3] ]
-  t2g2 = [-x for x in dat2[:,4] ]
-  eg2 = [-x for x in dat2[:,5] ]
-  d2 = [-(x[0]+x[1]) for x in zip(dat2[:,4],dat2[:,5]) ]
+  d2 = [-x for x in dat2[:,4] ]
 
   ax[2].plot(tot1,x1,color='k')
   ax[2].plot(s1,x1,color='r')
   ax[2].plot(p1,x1,color='b')
-  # ax[2].plot(t2g1,x1,color='y')
-  # ax[2].plot(eg1,x1,color='c')
   ax[2].plot(d1,x1,color='c')
 
   ax[0].plot(tot2,x2,color='k')
   ax[0].plot(s2,x2,color='r')
   ax[0].plot(p2,x2,color='b')
-  # ax[0].plot(t2g2,x2,color='y')
-  # ax[0].plot(eg2,x2,color='c')
   ax[0].plot(d2,x2,color='c')
 
   ax[2].axhline(y=0.0, xmin=point[0], xmax=point[count[0]-1], color='k', linestyle='--')
