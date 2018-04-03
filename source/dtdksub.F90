2db2b60fd90f69e2799cfa9bdab57568ed3574ef
Filipe Guimaraes <f.guimaraes@fz-juelich.de>
2018-04-03 14:52:37 +0200

    Added cycle in dtdk, as in hamiltk.

--- source/dtdksub.F90
+++ source/dtdksub.F90
@@ -1,24 +1,25 @@
 ! Calculate the derivative of H0 of a slab
 subroutine dtdksub(kp,dtdk)
-  use mod_f90_kind, only: double
+  use mod_f90_kind,  only: double
   use mod_constants, only: cZero, cI
-  use AtomTypes, only: NeighborIndex
-  use mod_system, only: s => sys
-  use TightBinding, only: nOrb
+  use AtomTypes,     only: NeighborIndex
+  use mod_system,    only: s => sys
+  use TightBinding,  only: nOrb
   use ElectricField, only: ElectricFieldVector
   implicit none
   integer :: i, j, k
   real(double), intent(in)  :: kp(3)
   complex(double),dimension(nOrb,nOrb,s%nAtoms,s%nAtoms),intent(out)  :: dtdk
   complex(double) :: kpExp
-  dtdk = cZero
 
+  dtdk = cZero
   ! Mouting derivative of slab's hamiltonian
   ! d/dk t_ij * exp(i*R*k) = i*R t_ij exp(i*R*k)
   do k = 1, s%nNeighbors
     j = s%Neighbors(k)%BasisIndex
     kpExp = cI * dot_product(ElectricFieldVector, s%Neighbors(k)%CellVector) * exp(cI * dot_product(kp,s%Neighbors(k)%CellVector))
     do i = 1, s%nAtoms
+      if(.not. sys%Neighbors(k)%isHopping(i)) cycle
       dtdk(1:nOrb,1:nOrb,j,i) = dtdk(1:nOrb,1:nOrb,j,i) + s%Neighbors(k)%t0i(1:nOrb, 1:nOrb, i) * kpExp
     end do
   end do
