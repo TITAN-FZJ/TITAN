5f99afb955677e13756c09299c7d5764bf9b8d5a
Filipe Guimaraes <filipesmg@gmail.com>
2020-07-15 19:43:05 +0200

    Many improvements on the plot_ldos.py script

--- scripts/plot_ldos.py
+++ scripts/plot_ldos.py
@@ -5,8 +5,10 @@ import matplotlib as mpl                      # Plotting library
 from matplotlib import rc                     # Improve math fonts
 from mpl_toolkits.axes_grid1 import AxesGrid  # Grid plotting
 import matplotlib.colors as colors            # Color selection and manipulation
-import scipy.interpolate                      # Interpolation library
+from scipy import integrate
 import argparse
+
+
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 ## for Palatino and other serif fonts use:
 # rc('font',**{'family':'serif','serif':['Palatino']})
@@ -36,6 +38,14 @@ ry2ev = 1.0
 
 parser = argparse.ArgumentParser(description="Parse bool")
 parser.add_argument("files", nargs="+", help="File(s) to plot")
+parser.add_argument('--orbitals', help='Array of integers', default="[]")
+parser.add_argument('--legends' , help='Array of legends', default="[]")
+parser.add_argument('--output', help='Output file', default="LDOS.pdf")
+parser.add_argument('--title' , help='Title of the graph', default="")
+parser.add_argument('--xlim' , help='Limit of x-axis', default="")
+parser.add_argument('--ylim' , help='Limit of y-axis', default="")
+parser.add_argument('--total' , default=False, action="store_true" , help='Plot the total DOS')
+parser.add_argument('--integrated' , default=False, action="store_true" , help='Plot the integrated DOS')
 parser.add_argument("--superconductivity", default=False, action="store_true" , help="Flag to plot the bands as for superconductors")
 parser.add_argument("--mev", default=False, action="store_true" , help="Plot superconductor bands in the same plot")
 args = parser.parse_args()
@@ -59,7 +69,7 @@ def read_header(file):
 ################################################################################
 # Get the data from the file and save it into a matrix
 ################################################################################
-def read_data(filename):
+def read_data(filename,orbitals=[]):
   # open a file using with statement
   data = []
   with open(filename,'r') as file:
@@ -67,7 +77,12 @@ def read_data(filename):
       # check if the current line
       # starts with "#"
       if not (line.startswith("#") or line.startswith(" #")):
-        data.append([float(x) for x in line.split()])
+        data_temp = [float(x) for x in line.split()]
+        # print(data_temp)
+        if len(orbitals) == 0:
+          data.append(data_temp)
+        else:
+          data.append([data_temp[0]] + [data_temp[x] for x in orbitals])
   ndata = np.array(data)
   return ndata
 
@@ -81,50 +96,103 @@ if __name__ == "__main__":
     labelx = r'$E-E_F$ [Ry]'
     labely = r'LDOS [states/Ry]'
 
-
-  numplots = len(args.files)
-  if numplots > 2:
-    sys.exit("Too many input files")
+  if args.total is False:
+    numplots = len(args.files)
+    if numplots > 2:
+      sys.exit("Too many input files")
+  else:
+    numplots = 1
 
   # Getting fermi energy from ldos up file
   fermi = read_header(args.files[0])
 
   if args.superconductivity:
-      # colors = brewer2mpl.get_map('Set1', 'qualitative', 5).mpl_colors
-      colors = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#a65628', '#b98600', '#000000']
-      # colors = np.array(['k', 'g', 'r', 'b']) # The colors you wish to cycle through
-      legends = np.array(['Total', r'$u_s$', r'$u_p$', r'$u_d$',r'$v_s$', r'$v_p$', r'$v_d$','', '', '', '','', '', ''])
+    # colors = brewer2mpl.get_map('Set1', 'qualitative', 5).mpl_colors
+    colors = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#a65628', '#b98600', '#000000']
+    # colors = np.array(['k', 'g', 'r', 'b']) # The colors you wish to cycle through
+    legends = np.array(['Total', r'$u_s$', r'$u_p$', r'$u_d$',r'$v_s$', r'$v_p$', r'$v_d$','', '', '', '','', '', ''])
   else:
-      # colors = brewer2mpl.get_map('Set1', 'qualitative', 5).mpl_colors
-      colors = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#a65628']
-      # colors = np.array(['k', 'g', 'r', 'b']) # The colors you wish to cycle through
-      legends = np.array(['Total', 's', 'p', 'd', '', '', '', ''])
+    # colors = brewer2mpl.get_map('Set1', 'qualitative', 5).mpl_colors
+    colors = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#a65628']
+    # colors = np.array(['k', 'g', 'r', 'b']) # The colors you wish to cycle through
+    legends = np.array(['Total', 's', 'p', 'd', '', '', '', ''])
+
+
+  # Getting the orbitals to plot...
+  orbitals = eval(args.orbitals)
+
+  # ...and their legends
+  if args.legends != "[]":
+    legends = [r"{}".format(string) for string in args.legends.split(",")]
+    if len(legends) != len(orbitals):
+      sys.exit("Incorrect number of legends. Please provide one for each orbital. len(legends) = " + str(len(legends)) + ", len(orbitals) = " + str(len(orbitals)))
+    if numplots == 2:
+      for i in range(len(orbitals)):
+        legends.append(r"")
+
 
   fig, ax = plt.subplots(1, 1, figsize=(6, 1 + 1.5*numplots))
 
   plt.axhline(0.0, color='k', linewidth=0.75)
 
+  data = []
+  data_int = []
+  if args.total is False:
+    for j in range(numplots):
+      data.append(read_data(args.files[j],orbitals=orbitals))
+      data[j] = data[j][data[j][:,0].argsort()]
+      if args.integrated is True:
+        data_int.append(np.array([integrate.cumtrapz(data[j][:,k], data[j][:,0], initial=0) for k in range(1,len(data[j][0,:])) ]).transpose())
+  else:
+    data.append(read_data(args.files[0],orbitals=orbitals))
+    for j in range(1,len(args.files)):
+      data_temp = read_data(args.files[j],orbitals=orbitals)
+      for i, line in enumerate(data[0]):
+        data[0][i] = np.array([data_temp[i,0]] + [data[0][i,k] + data_temp[i,k] for k in range(1,len(data_temp[i,:]))])
+    data[0] = data[0][data[0][:,0].argsort()]
+    if args.integrated is True:
+      data_int.append(np.array([integrate.cumtrapz(data[0][:,k], data[0][:,0], initial=0) for k in range(1,len(data[0][0,:])) ]).transpose())
+
+
   a1 = []
   for j in range(numplots):
-    data = read_data(args.files[j])
+    # data = read_data(args.files[j],orbitals=orbitals)
     # datau = np.loadtxt(filenameu)
-    data = data[data[:,0].argsort()]
-    x = [(data[k,0]-fermi)*ry2ev for k in range(len(data[:,0]))]
+    x = [(data[j][k,0]-fermi)*ry2ev for k in range(len(data[j][:,0]))]
     signal = (-1)**j
-    for i in range(1,len(data[0,:])):
-      y = [signal*data[k,i]/ry2ev for k in range(len(data[:,i]))]
-      plt.plot(x, y, color=colors[i-1], label=legends[j*(len(data[0,:])-1)+(i-1)])
+    for i in range(1,len(data[j][0,:])):
+      y = [signal*data[j][k,i]/ry2ev for k in range(len(data[j][:,i]))]
+      plt.plot(x, y, color=colors[i-1], label=legends[j*(len(data[j][0,:])-1)+(i-1)])
+    # Plotting integrated DOS
+    if args.integrated is True:
+      signal = (-1)**j
+      for i in range(0,len(data_int[j][0,:])):
+        y = [signal*data_int[j][k,i]/ry2ev for k in range(len(data_int[j][:,i]))]
+        plt.plot(x, y, ls='--', color=colors[i], label=(legends[j*(len(data_int[j][0,:]))+i]+" int" if legends[j*(len(data_int[j][0,:]))+i] != "" else "") )
 
-    a1.append(max(data[:,1]/ry2ev))
+
+    a1.append(max(data[j][:,1]/ry2ev))
   max_ldos = max(a1)
 
-  ylim = [(-1.1*abs(max_ldos) if numplots > 1 else 0.0),1.1*abs(max_ldos)]
+  if args.xlim != "":
+    xlim = [-eval(args.xlim),eval(args.xlim)]
+    plt.xlim(xlim)
+
+  if args.ylim != "":
+    ylim = [(-eval(args.ylim) if numplots > 1 else 0.0),eval(args.ylim)]
+  else:
+    ylim = [(-1.1*abs(max_ldos) if numplots > 1 else 0.0),1.1*abs(max_ldos)]
+
   plt.ylim(ylim)
 
 
   plt.xlabel(labelx)
   plt.ylabel(labely)
-  plt.title('LDOS')
+  if args.title == "":
+    plt.title('LDOS')
+  else:
+    plt.title(args.title)
+
   plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
   # if(Ef != 0.0):
@@ -133,7 +201,6 @@ if __name__ == "__main__":
   # fig = plt.gcf()
   # fig.set_size_inches(3., 2.2)
   plt.tight_layout()
-  plt.savefig('LDOS.pdf')
-  # plt.savefig(filename,transparent=True,dpi=2540, bbox_inches='tight')
+  plt.savefig(args.output)
 
   plt.show()
