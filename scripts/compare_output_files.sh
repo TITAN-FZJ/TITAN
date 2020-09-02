060865fcff1725fa1ad23d5ca32222404be26b68
Uriel Aceves <uri.fisico@ciencias.unam.mx>
2020-09-02 15:37:10 +0200

    Format modifications, now the output for the mag. mom., gap parameter, and orb. mom. only says if there is a difference or not. The rest of the outputs continue to show the lines where it changes

--- scripts/compare_output_files.sh
+++ scripts/compare_output_files.sh
@@ -9,7 +9,7 @@
 file1="$1"
 file2="$2"
 
-printf "\nI am comparing the files $1 and $2\n\n"
+printf "\nI am comparing the files $1 and $2\n"
 
 ###############################################
 printf "Fermi energy\n"
@@ -19,7 +19,7 @@ dummy="Self-consistent ground state"
 diff <(grep "$dummy" -A 1 $file1) <(grep "$dummy" -A 1 $file2) > /dev/null
 
 if [ $? -eq 0 ]; then
-    printf ">>>The Fermi energies are identical\n\n"
+    printf ">>>The Fermi energies are identical\n"
 else
     diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2)
 fi
@@ -31,7 +31,7 @@ dummy="Charge density"
 diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2) > /dev/null
 
 if [ $? -eq 0 ]; then
-    printf ">>>The charge densities are identical\n\n"
+    printf ">>>The charge densities are identical\n"
 else
     diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2)
 fi
@@ -43,9 +43,10 @@ dummy="Averages of the norms per orbital type"
 diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2) > /dev/null
 
 if [ $? -eq 0 ]; then
-    printf ">>>The gap parameters are identical\n\n"
+    printf ">>>The gap parameters are identical\n"
 else
-    diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2)
+    printf ">>>These results are different, it can be problem of -0.0 != 0.0\n"
+    #diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2)
 fi
 ###############################################
 printf "\nMagnetization components\n"
@@ -55,9 +56,10 @@ dummy="Magnetization components"
 diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2) > /dev/null
 
 if [ $? -eq 0 ]; then
-    printf ">>>The magnetization components are identical\n\n"
+    printf ">>>The magnetization components are identical\n"
 else
-    diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2)
+    printf ">>>These results are different, it can be problem of -0.0 != 0.0\n"
+    #diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2)
 fi
 ###############################################
 printf "\nOrbital components\n"
@@ -67,9 +69,10 @@ dummy="Orbital components in global frame"
 diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2) > /dev/null
 
 if [ $? -eq 0 ]; then
-    printf ">>>The orbital components are identical\n\n"
+    printf ">>>The orbital components are identical\n"
 else
-    diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2)
+    printf ">>>These results are different, it can be problem of -0.0 != 0.0\n"
+    #diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2)
 fi
 ###############################################
 printf "\nTotal\n"
@@ -79,14 +82,16 @@ dummy="Total"
 diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2) > /dev/null
 
 if [ $? -eq 0 ]; then
-    printf ">>>The total results are identical\n\n"
+    printf ">>>The total results are identical\n"
 else
     diff <(grep "$dummy" -A 11 $file1) <(grep "$dummy" -A 11 $file2)
 fi
 ###############################################
+printf "\n"
 dummy="Time after self-consistency"
 grep "$dummy" $file1
 grep "$dummy" $file2
+printf "\n"
 dummy="Finished on"
 grep "$dummy" $file1
 grep "$dummy" $file2
