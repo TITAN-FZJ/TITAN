46646b059686b50205ffe0a1ecf61f077fb222ba
Uriel Aceves <uri.fisico@ciencias.unam.mx>
2022-12-16 15:44:13 +0100

    Corrected the supercondicting term of Jij
    Since the equations were wrong for the metallic case when calculating
    Jij, they were also wrong for the superconducting term. I made the
    required changes.
    
    I cleaned debugging comments that I had been adding since I started
    debugging the jij_energy.F90 file. More commenting needs to be done.
    
    I added a section to print exclusively the contributions from the
    superconducting term, so we can access it easily.

--- source/jij_energy.F90
+++ source/jij_energy.F90
@@ -19,7 +19,8 @@ subroutine jij_energy(q,Jij)
   implicit none
   real(dp),    dimension(3),                     intent(in)  :: q
   complex(dp), dimension(s%nAtoms,s%nAtoms,3,3), intent(out) :: Jij
-  complex(dp), dimension(s%nAtoms,s%nAtoms,3,3)              :: Jijint
+  complex(dp), dimension(s%nAtoms,s%nAtoms,3,3)              :: Jijs
+  complex(dp), dimension(s%nAtoms,s%nAtoms,3,3)              :: Jijint, Jijsint
   integer(int64) :: ix
   integer(int32) :: i,j,mu,nu,alpha,nOrb2_i,nOrb2_j
   real(dp) :: kp(3),kmq(3),kpq(3),ep
@@ -28,29 +29,23 @@ subroutine jij_energy(q,Jij)
   complex(dp), dimension(s%nAtoms,3,s%nOrb2,s%nOrb2)        :: dBxc_dm
   complex(dp), dimension(s%nAtoms,3,3,s%nOrb2,s%nOrb2)      :: d2Bxc_dm2
   complex(dp), dimension(s%nAtoms,s%nOrb2,s%nOrb2)          :: paulievec
-  complex(dp), dimension(s%nOrb2,s%nOrb2)                   :: gij,gji,gijm,gjim,gjip,temp1,temp1m,temp1p,temp2,paulia,paulib,pauli_star
-  complex(dp), dimension(s%nOrb2sc,s%nOrb2sc,s%nAtoms,s%nAtoms) :: gf,gfmq,gfpq, gfm, gfmqm,gfpqm
-  complex(dp), dimension(s%nOrb2,s%nOrb2)                       :: geh, ghem, ghep
+  complex(dp), dimension(s%nOrb2,s%nOrb2)                   :: gij,gji,gijm,gjim,temp1,temp1m,temp1p,temp2,paulia,paulib,pauli_star_a,pauli_star_b
+  complex(dp), dimension(s%nOrb2sc,s%nOrb2sc,s%nAtoms,s%nAtoms) :: gf,gfmq,gfm, gfmqm,gfpqm
+  complex(dp), dimension(s%nOrb2,s%nOrb2)                       :: gehij,gehji, ghemij,ghemji, ghep
+  ! To Do: Put the three arrays below in the same format as the ones above
   complex(dp) :: Jijk(s%nAtoms,s%nAtoms,3,3)
+  complex(dp) :: Jijks(s%nAtoms,s%nAtoms,3,3)
   complex(dp) :: Jijkan(s%nAtoms,3,3)
   integer :: kap, kip, count ! To be deleted
   integer :: ncount, ki
+
+  ! THIS FILE NEEDS TO BE PROPERLY COMMENTED 
 
   external :: zgemm,MPI_Reduce
 
   fermi = merge(0._dp,s%Ef,lsuperCond)
 
   ncount = s%nAtoms * s%nAtoms * 3 * 3
-
-  !To delete
-  ! if(activeRank == 0) then
-  !   write(*,*) "Matrix"
-  !   write(*,*) s%Types(s%Basis(1)%Material)%pauli_dorb(2,1:18,1:18)
-  ! end if
-  !
-  !To delete
-  ! s%Types(s%Basis(1)%Material)%pauli_dorb(2,1:18,1:18) = cI*s%Types(s%Basis(1)%Material)%pauli_dorb(1,1:18,1:18)
-  !
 
   do i = 1, s%nAtoms
     nOrb2_i = s%Types(s%Basis(i)%Material)%nOrb2
@@ -80,50 +75,19 @@ subroutine jij_energy(q,Jij)
   if((.not.llineargfsoc) .and. (.not.llinearsoc)) call hamilt_local(s)
 
   ! Calculating Jij using a complex integral
-  Jij = cZero
-
-  ! if(activeRank == 0) then
-  !   write(*,*) "Inside ", q
-  !   do kap = 1, s%nOrb2
-  !     do kip = 1, s%nOrb2
-  !       if(q(1)==0._dp .and. q(2)==0._dp) then
-  !         write(*,*) dBxc_dm(6,1,kap,kip) ! This line is targeting the 6th atom
-  !       end if
-  !     end do
-  !   end do
-  ! end if
+  Jij  = cZero
+  Jijs = cZero
 
   !$omp parallel default(none) &
-  !$omp& private(ix,i,j,mu,nu,nOrb2_i,nOrb2_j,alpha,kp,ep,weight,kmq,kpq,gf,gfm,gfmq,gfpq,gij,gji,gijm,gjim,gjip,paulia,paulib,pauli_star,temp1,temp1m,temp1p,temp2,Jijkan,Jijk,Jijint,geh,ghem,ghep) &
-  !$omp& shared(activeRank, count, s,fermi,lsuperCond,eta,y,wght,q,local_points,dBxc_dm,d2Bxc_dm2,Jij,bzs,E_k_imag_mesh,paulievec)
+  !$omp& private(ix,i,j,mu,nu,nOrb2_i,nOrb2_j,alpha,kp,ep,weight,kmq,kpq,gf,gfm,gfmq,gij,gji,gijm,gjim,paulia,paulib,pauli_star_b,pauli_star_a,temp1,temp1m,temp1p,temp2,Jijkan,Jijk,Jijks,Jijint,Jijsint,gehij,gehji,ghemij,ghemji,ghep) &
+  !$omp& shared(activeRank, count, s,fermi,lsuperCond,eta,y,wght,q,local_points,dBxc_dm,d2Bxc_dm2,Jij,Jijs,bzs,E_k_imag_mesh,paulievec)
 
   Jijint = cZero
+  Jijsint = cZero
   temp1 = cZero
   temp1m = cZero 
   temp1p = cZero 
   temp2 = cZero
-
-
-  ! write(*,*) s%nAtoms
-  !To be deleted
-  ! open(unit=35012, file='dbxc.dat', status = 'unknown')
-  ! if(activeRank == 0) then
-  !   if(q(0)==0._dp .and. q(1)==0._dp) then
-  !     write(*,*) dBxc_dm(6,1,:,:) ! This line is targeting the 6th atom
-  !   end if
-  ! end if
-  ! write(42070,*) dBxc_dm(6,2,:,:) ! This line is targeting the 6th atom
-  ! write(42070,*) dBxc_dm(6,3,:,:) ! This line is targeting the 6th atom
-  ! close(35012)
-  !
-
-  !!!!
-  ! This is just for trials
-  ! dBxc_dm = cZero 
-  ! do kap = 1,9
-  !   dBxc_dm(1:s%nAtoms,1:3,kap,kap) = cOne 
-  ! end do
-  !!!!
 
   !$omp do schedule(dynamic)
   do ix = 1, local_points
@@ -132,69 +96,51 @@ subroutine jij_energy(q,Jij)
       weight = wght(E_k_imag_mesh(1,ix)) * bzs(E_k_imag_mesh(1,ix))%w(E_k_imag_mesh(2,ix))
       ! Green function on energy Ef + iy, and wave vector kp
       call green(fermi,ep+eta,s,kp,gf)
-      ! Green function on energy Ef - iy, and wave vector kp
-      ! call green(fermi,ep-eta,s,kp,gfm)
 
       ! Green function on energy Ef + iy, and wave vector kp-q
       kmq = kp-q
       call green(fermi,ep+eta,s,kmq,gfmq)
-      kpq = kp+q
-      call green(fermi,ep+eta,s,kpq,gfpq)
-
-      !To delete
-      ! if(activeRank==0) then
-      !   ! write(*,*) "minus ", kmq
-      !   ! write(*,*) "plus ", kpq  
-      ! !   write(*,*) gfpq(1:9,10:18,1,1)
-      ! !   write(*,*) gfpq(10:18,1:9,1,1)
-      ! end if
-      !
+      ! kpq = kp+q
+      ! call green(fermi,ep+eta,s,kpq,gfpq)
 
       Jijk   = 0._dp
+      Jijks  = 0._dp
       Jijkan = 0._dp
       do j = 1,s%nAtoms
         do i = 1,s%nAtoms
           nOrb2_j = s%Types(s%Basis(j)%Material)%nOrb2
           nOrb2_i = s%Types(s%Basis(i)%Material)%nOrb2
+          ! G_{ij}(k)
           gij = gf (1:nOrb2_i,1:nOrb2_j,i,j)
+          ! G_{ji}(k)
           gji = gf (1:nOrb2_i,1:nOrb2_j,j,i)
+          ! G_{ij}(k-q)
           gijm = gfmq(1:nOrb2_i,1:nOrb2_j,i,j)
+          ! G_{ji}(k-q)
           gjim = gfmq(1:nOrb2_j,1:nOrb2_i,j,i)
-          gjip = gfpq(1:nOrb2_j,1:nOrb2_i,i,j)
+          ! gjip = gfpq(1:nOrb2_j,1:nOrb2_i,i,j)
+
           if (lsuperCond) then
-            geh(1:nOrb2_i,1:nOrb2_j) = gf (1:nOrb2_i,nOrb2_j+1:2*nOrb2_j,i,j)
-            ghem(1:nOrb2_j,1:nOrb2_i) = gfmq(nOrb2_j+1:2*nOrb2_j,1:nOrb2_i,j,i)
+
+            ! If we think about the BdG Green functions as comprised by four blocks
+            ! the matrix would look like this
+            ! G_{ij}(k) = | G^{ee}(k) G^{eh}(k) |
+            !             | G^{he}(k) G^{hh}(k) |.
+            ! This is why the Green functions are taken as below 
+
+            ! G_{ij}^{eh}(k)
+            gehij(1:nOrb2_i,1:nOrb2_j) = gf (1:nOrb2_i,nOrb2_j+1:2*nOrb2_j,i,j)
+            ! G_{ji}^{eh}(k)
+            gehji(1:nOrb2_i,1:nOrb2_j) = gf (1:nOrb2_j,nOrb2_i+1:2*nOrb2_i,j,i)
+            ! G_{ij}^{he}(k-q)
+            ghemij(1:nOrb2_i,1:nOrb2_j) = gfmq(nOrb2_i+1:2*nOrb2_i,1:nOrb2_j,i,j)
+            ! G_{ji}^{he}(k-q)
+            ghemji(1:nOrb2_j,1:nOrb2_i) = gfmq(nOrb2_j+1:2*nOrb2_j,1:nOrb2_i,j,i)
+
           end if
 
           do nu = 1,3
             do mu = 1,3
-              ! paulia(1:nOrb2_i,1:nOrb2_i)= dBxc_dm(i,mu,1:nOrb2_i,1:nOrb2_i)
-              ! paulib(1:nOrb2_j,1:nOrb2_j)= dBxc_dm(j,nu,1:nOrb2_j,1:nOrb2_j)
-              ! !To delete
-              ! ! paulia(1:nOrb2_i,1:nOrb2_i)= s%Types(s%Basis(i)%Material)%pauli_dorb(mu,1:nOrb2_i,1:nOrb2_i)
-              ! ! paulib(1:nOrb2_j,1:nOrb2_j)= s%Types(s%Basis(j)%Material)%pauli_dorb(nu,1:nOrb2_i,1:nOrb2_i)
-              ! !
-              ! ! temp1 = paulia * gij = dBxc_dm_i * gij
-              ! call zgemm('n','n',nOrb2_i,nOrb2_j,nOrb2_i,cOne,paulia,s%nOrb2,gij,   s%nOrb2,cZero,temp1,s%nOrb2)
-              ! ! temp2 = temp1 * paulib = paulia * gij * paulib = dBxc_dm_i * gij * dBxc_dm_j
-              ! call zgemm('n','n',nOrb2_i,nOrb2_j,nOrb2_j,cOne,temp1, s%nOrb2,paulib,s%nOrb2,cZero,temp2,s%nOrb2)              
-              ! ! temp1m = temp2 * gji = paulia * gij * paulib * gji = dBxc_dm_i * gij(k-q) * dBxc_dm_j * gji
-              ! call zgemm('n','n',nOrb2_i,nOrb2_i,nOrb2_j,cOne,temp2, s%nOrb2,gjim,   s%nOrb2,cZero,temp1m,s%nOrb2)
-              ! ! temp1p = temp2 * gji = paulia * gij * paulib * gji = dBxc_dm_i * gij(k+q) * dBxc_dm_j * gji
-              ! call zgemm('n','n',nOrb2_i,nOrb2_i,nOrb2_j,cOne,temp2, s%nOrb2,gjip,   s%nOrb2,cZero,temp1p,s%nOrb2)
-              ! !To delete
-              ! ! if(nu==2 .and. mu==1 .and. activeRank==0) then
-              ! !   write(*,*) temp1p(:,:)
-              ! !   ! do count=1,18
-              ! !   !   write(*,*) temp1p(count,count)
-              ! !   ! end do
-              ! ! end if
-              !
-              ! Trying new term for the conjugate
-              ! call zgemm('c','c',nOrb2_i,nOrb2_j,nOrb2_i,cOne,gjim,s%nOrb2,paulib,   s%nOrb2,cZero,temp1,s%nOrb2)
-              ! call zgemm('n','c',nOrb2_i,nOrb2_j,nOrb2_i,cOne,temp1,s%nOrb2,gij,   s%nOrb2,cZero,temp2,s%nOrb2)
-              ! call zgemm('n','c',nOrb2_i,nOrb2_j,nOrb2_i,cOne,temp2,s%nOrb2,paulia,   s%nOrb2,cZero,temp1p,s%nOrb2)
-
               paulia(1:nOrb2_i,1:nOrb2_i)= dBxc_dm(i,mu,1:nOrb2_i,1:nOrb2_i)
               paulib(1:nOrb2_j,1:nOrb2_j)= dBxc_dm(j,nu,1:nOrb2_j,1:nOrb2_j)
               ! temp1 = paulia * gij = dBxc_dm_i * gij
@@ -204,9 +150,7 @@ subroutine jij_energy(q,Jij)
               ! temp1m = temp2 * gji = paulia * gij * paulib * gji = dBxc_dm_i * gij(k-q) * dBxc_dm_j * gji
               call zgemm('n','n',nOrb2_i,nOrb2_i,nOrb2_j,cOne,temp2, s%nOrb2,gjim,   s%nOrb2,cZero,temp1m,s%nOrb2)
 
-
-              ! paulia(1:nOrb2_i,1:nOrb2_i)= dBxc_dm(i,nu,1:nOrb2_i,1:nOrb2_i)
-              ! paulib(1:nOrb2_j,1:nOrb2_j)= dBxc_dm(j,mu,1:nOrb2_j,1:nOrb2_j)
+              ! Second term of the equation
               ! temp1 = paulia * gij = dBxc_dm_i * gij
               call zgemm('n','n',nOrb2_i,nOrb2_j,nOrb2_i,cOne,paulia,s%nOrb2,gijm,   s%nOrb2,cZero,temp1,s%nOrb2)
               ! temp2 = temp1 * paulib = paulia * gij * paulib = dBxc_dm_i * gij * dBxc_dm_j
@@ -217,30 +161,37 @@ subroutine jij_energy(q,Jij)
               ! Trace over orbitals and spins
               do alpha = 1,nOrb2_i
                 Jijk(i,j,mu,nu) = Jijk(i,j,mu,nu) + temp1m(alpha,alpha) + conjg(temp1p(alpha,alpha))
-                ! Jijk(i,j,mu,nu) = Jijk(i,j,mu,nu) + temp1m(alpha,alpha) + temp1p(alpha,alpha)
-                ! if(activeRank==0 .and. nu==2 .and. mu==1) then
-                !   write(*,*) Jijk(i,j,mu,nu) , temp1m(alpha,alpha) , conjg(temp1p(alpha,alpha))
-                ! end if
-                ! Jijk(i,j,mu,nu) = Jijk(i,j,mu,nu) + 0.5_dp*(temp1m(alpha,alpha) + temp1p(alpha,alpha))
               end do
+
+              ! When superconductivity is on, there is a correction to the magnetic exchange interactions
+              ! that comes from the electron-hole interactions
               if ( lsuperCond ) then
-                pauli_star(1:nOrb2_j,1:nOrb2_j) = conjg(paulib(1:nOrb2_j,1:nOrb2_j))
+
+                pauli_star_a(1:nOrb2_i,1:nOrb2_i) = conjg(paulia(1:nOrb2_i,1:nOrb2_i))
+                pauli_star_b(1:nOrb2_j,1:nOrb2_j) = conjg(paulib(1:nOrb2_j,1:nOrb2_j))
                 ! temp1 = paulia * geh(ij) = dBxc_dm_i * geh(ij)
-                call zgemm('n','n',nOrb2_i,nOrb2_j,nOrb2_i,cOne,paulia,s%nOrb2,geh       ,s%nOrb2,cZero,temp1,s%nOrb2)
+                call zgemm('n','n',nOrb2_i,nOrb2_j,nOrb2_i,cOne,paulia,s%nOrb2,gehij     ,s%nOrb2,cZero,temp1,s%nOrb2)
                 ! temp2 = temp1 * pauli_star = dBxc_dm_i * geh(ij) * (dBxc_dm_j)^*
-                call zgemm('n','n',nOrb2_i,nOrb2_j,nOrb2_j,cOne,temp1 ,s%nOrb2,pauli_star,s%nOrb2,cZero,temp2,s%nOrb2)
+                call zgemm('n','n',nOrb2_i,nOrb2_j,nOrb2_j,cOne,temp1 ,s%nOrb2,pauli_star_b,s%nOrb2,cZero,temp2,s%nOrb2)
                 ! temp1m = temp2 * ghe(ji) = dBxc_dm_i * geh(ij) * (dBxc_dm_j)^* * ghe(ji)(k-q)
-                call zgemm('n','n',nOrb2_i,nOrb2_i,nOrb2_j,cOne,temp2 ,s%nOrb2,ghem      ,s%nOrb2,cZero,temp1m,s%nOrb2)
+                call zgemm('n','n',nOrb2_i,nOrb2_i,nOrb2_j,cOne,temp2 ,s%nOrb2,ghemji     ,s%nOrb2,cZero,temp1m,s%nOrb2)
+
+                ! temp1 = paulia * geh(ij) = dBxc_dm_i * geh(ij)
+                call zgemm('n','n',nOrb2_i,nOrb2_j,nOrb2_i,cOne,pauli_star_a,s%nOrb2,ghemij     ,s%nOrb2,cZero,temp1,s%nOrb2)
+                ! temp2 = temp1 * pauli_star = dBxc_dm_i * geh(ij) * (dBxc_dm_j)^*
+                call zgemm('n','n',nOrb2_i,nOrb2_j,nOrb2_j,cOne,temp1 ,s%nOrb2,paulib,s%nOrb2,cZero,temp2,s%nOrb2)
                 ! temp1p = temp2 * ghe(ji) = dBxc_dm_i * geh(ij) * (dBxc_dm_j)^* * ghe(ji)(k+q)
-                call zgemm('n','n',nOrb2_i,nOrb2_i,nOrb2_j,cOne,temp2 ,s%nOrb2,ghep      ,s%nOrb2,cZero,temp1p,s%nOrb2)
+                call zgemm('n','n',nOrb2_i,nOrb2_i,nOrb2_j,cOne,temp2 ,s%nOrb2,gehji      ,s%nOrb2,cZero,temp1p,s%nOrb2)
+
                 do alpha = 1,nOrb2_i
-                  Jijk(i,j,mu,nu) = Jijk(i,j,mu,nu) - temp1m(alpha,alpha) - conjg(temp1p(alpha,alpha))
-                  !Jijk(i,j,mu,nu) = temp1m(alpha,alpha) + conjg(temp1p(alpha,alpha))
+                  Jijks(i,j,mu,nu) = Jijks(i,j,mu,nu) - temp1m(alpha,alpha) - conjg(temp1p(alpha,alpha))
                 end do
+
               end if
             end do
           end do
 
+          ! The section below should probably be deleted
           ! Anisotropy (on-site) term
           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
           ! Commented out for the moment
@@ -263,18 +214,16 @@ subroutine jij_energy(q,Jij)
         end do
       end do
 
-      Jijint = Jijint + Jijk * weight
-      !To delete
-      ! if(activeRank==0) then
-      !   write(*,*) Jijint, Jijk, weight, ix, q
-      ! end if
-      !
+      Jijint = Jijint + Jijk * weight + Jijks * weight
+      Jijsint = Jijsint + Jijks * weight
   end do
   !$omp end do nowait
 
   Jijint = -Jijint/tpi
+  Jijsint = -Jijsint/tpi
   !$omp critical
-    Jij = Jij + Jijint
+    Jij  = Jij + Jijint
+    Jijs = Jijs + Jijsint
   !$omp end critical
   !$omp end parallel
 
@@ -283,6 +232,16 @@ subroutine jij_energy(q,Jij)
   else
     call MPI_Reduce(Jij, Jij, ncount, MPI_DOUBLE_COMPLEX, MPI_SUM, 0, activeComm, ierr)
   end if
-  !call MPI_Allreduce(MPI_IN_PLACE, Jij, ncount, MPI_DOUBLE_COMPLEX, MPI_SUM, activeComm, ierr)
 
+  if(activeRank == 0) then
+    call MPI_Reduce(MPI_IN_PLACE, Jijs, ncount, MPI_DOUBLE_COMPLEX, MPI_SUM, 0, activeComm, ierr)
+  else
+    call MPI_Reduce(Jijs, Jijs, ncount, MPI_DOUBLE_COMPLEX, MPI_SUM, 0, activeComm, ierr)
+  end if
+
+  !To be deleted
+  open(unit=35012, file='Jijs.dat', status = 'unknown')
+  write(35012,*) weight, q, Jijs
+  close(35012)
+  
 end subroutine jij_energy
